84be5a9dad9971aaea4b02fcc4530efad675df03
Yoshitaka motomura <stay.daybreak@gmail.com>
2023-10-23 01:14:59 +0900

    Added: README.md

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 import os
+from pathlib import Path
 from dotenv import load_dotenv
+from google.oauth2.credentials import Credentials
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
 
 load_dotenv()
 
+EMAIL = (os.environ['WORK_MAIL_ADDRESS'])
+JSON_FILE = (os.environ['CREDENTIAL_JSON_FILE'])
+
+
 def main():
-    print("test")
+    credentials_file = Path.cwd().joinpath('credentials', JSON_FILE)
+    credentials = service_account.Credentials.from_service_account_file(
+        credentials_file,
+        scopes=['https://www.googleapis.com/auth/drive'],
+    )
+
+    delegated_credentials = credentials.with_subject(EMAIL)
+
+    drive_service = build('drive', 'v3', credentials=delegated_credentials)
+
+    results = drive_service.files().list(q="sharedWithMe=true", fields="files(id, name, webViewLink)").execute()
+    items = results.get('files', [])
+
+    if not items:
+        print('No shared files found.')
+    else:
+        print('Shared files:')
+        for item in items:
+            print(f"ID: {item['id']}, Name: {item['name']}, Link: {item['webViewLink']}")
 
 
 if __name__ == '__main__':
